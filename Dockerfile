abd15b14de1b9cd24af221fa77312493e7ad6cc7
Jens Walter <jens@apimeister.com>
2020-09-08 13:30:35 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,23 @@
-# FROM gentoo/stage3-amd64 AS builder
-
-# prepare builder
-# RUN emerge --sync
-# RUN emerge --oneshot sys-apps/portage
-# RUN emerge --oneshot sandbox
-# RUN emerge rust
 FROM rust:1.46.0-alpine AS builder
+# prepare builder
 RUN apk add --update gcc g++ pkgconfig openssl-dev
-# RUN apk add --update curl gcc g++ pkgconfig perl make musl musl-dev apk-tools-static
-# RUN apt-get update && apt-get install -y pkg-config musl musl-tools libssl-dev
-# RUN ln -s /usr/include/x86_64-linux-gnu/openssl/opensslconf.h /usr/include/openssl/opensslconf.h 
-# WORKDIR /
-#RUN ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/x86_64-linux-musl/asm &&     ln -s /usr/include/asm-generic /usr/include/x86_64-linux-musl/asm-generic &&     ln -s /usr/include/linux /usr/include/x86_64-linux-musl/linux
-# RUN mkdir /musl
-# RUN wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1g.tar.gz
-# RUN tar zxvf OpenSSL_1_1_1g.tar.gz 
-# WORKDIR /openssl-OpenSSL_1_1_1g
-# RUN CC="musl-gcc -fPIE -pie" ./Configure no-shared no-async --prefix=/musl --openssldir=/musl/ssl linux-x86_64
-# RUN make depend
-# RUN make -j4
-# RUN make install
 
-# ENV PKG_CONFIG_ALLOW_CROSS=1
-# ENV OPENSSL_STATIC=true
-# ENV OPENSSL_DIR=/musl
-
-WORKDIR /usr/src/
-# RUN rustup target add x86_64-unknown-linux-musl
 # build project
+WORKDIR /usr/src/
 RUN USER=root cargo new tibco-ems-operator
 WORKDIR /usr/src/tibco-ems-operator
 COPY Cargo.toml .
-# RUN cargo tree
-#RUN cargo build --release --target x86_64-unknown-linux-musl
-# RUN cargo build
 COPY src ./src
 RUN cargo install --path .
+# RUN cargo build
+
+# CMD ["/bin/sh"]
+
 
 # Bundle Stage
-FROM alpine
-RUN apk add libgcc libc6-compat
-RUN ln -s /lib/libc.musl-x86_64.so.1 /lib/ld-linux-x86-64.so.2
+# FROM alpine
+FROM centos
 COPY --from=builder /usr/local/cargo/bin/tibco-ems-operator .
-USER 1000
+COPY bin/libtibemsadmin64.so bin/libtibems64.so /usr/lib
+
 CMD ["./tibco-ems-operator"]
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,60 @@
 extern crate serde_derive;
 use kube::{api::{Api, ListParams, Meta, WatchEvent}, Client};
-use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
 use futures::{StreamExt, TryStreamExt};
 use serde::{Serialize, Deserialize};
 use kube_derive::CustomResource;
+use kube::config::Config;
 
 #[derive(CustomResource, Serialize, Deserialize, Default, Clone, Debug)]
 #[kube(group = "tibcoems.apimeister.com", version = "v1", kind="Queue", namespaced)]
 pub struct QueueSpec {
     pub name: String,
+    pub global: Option<bool>,
+    pub max_msg: Option<u32>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), kube::Error>  {
-    let client = Client::try_default().await?;
-    // Set a namespace. We're just hard-coding for now.
-    let namespace = "default";
+    println!("infering client config");
+    let config = Config::infer().await;
+    println!("infering client config done");
+    match config {
+        Err(e) => println!("error {}", e),
+        Ok(c) => {
+            println!("success ");
+            let client = Client::new(c);
+            println!("initializing client");
+            //config.accept_invalid_certs = true;
+            // let client = Client::try_default().await?;
+            // Set a namespace. We're just hard-coding for now.
+            let namespace = "default";
 
-    let crds: Api<CustomResourceDefinition> = Api::namespaced(client, namespace);
-    let lp = ListParams::default().fields("apiVersion=tibcoems.apimeister.com").timeout(20);
+            println!("connecting");
+            let crds: Api<Queue> = Api::namespaced(client, namespace);
+            let lp = ListParams::default();
 
-    let mut stream = crds.watch(&lp, "0").await?.boxed();
-    while let Some(status) = stream.try_next().await? {
-        match status {
-            WatchEvent::Added(s) => println!("Added {}", Meta::name(&s)),
-            WatchEvent::Modified(s) => println!("Modified: {}", Meta::name(&s)),
-            WatchEvent::Deleted(s) => println!("Deleted {}", Meta::name(&s)),
-            WatchEvent::Error(s) => println!("{}", s),
-            _ => {}
+            println!("streaming events ...");
+            let mut stream = crds.watch(&lp, "0").await?.boxed();
+            while let Some(status) = stream.try_next().await? {
+                match status {
+                    WatchEvent::Added(queue) => println!("Added {}", Meta::name(&queue)),
+                    WatchEvent::Modified(queue) => {
+                        println!("Modified: {}", Meta::name(&queue));
+                        println!("{}",queue.spec.name);
+                        match queue.spec.global{
+                            Some(g) => println!("isGlobal: {}",g),
+                            None => println!("global not set"),
+                        }
+                        let json_str = serde_json::to_string_pretty( &queue);
+                        println!("{}",json_str.unwrap());
+                    },
+                    WatchEvent::Deleted(queue) => println!("Deleted {}", Meta::name(&queue)),
+                    WatchEvent::Error(queue) => println!("error: {}", queue),
+                    _ => {}
+                }
+            }
         }
     }
+
     Ok(())
 }
