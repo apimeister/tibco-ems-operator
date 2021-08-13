02b3ec0066b54a86d84346781208b9494f400684
Schoksen <Schoksen@hotmail.de>
2021-08-13 13:45:50 +0200

    refactor if hell into match statement

--- src/main.rs
+++ src/main.rs
@@ -19,45 +19,10 @@ extern crate env_var;
 async fn respond(req: Request<Body>) -> Result<Response<Body>> {
   let uri = req.uri().path();
   trace!("{} {}",req.method(),uri);
-  if uri == "/metrics" {
-    let mut body = "".to_owned();
-    body.push_str("# TYPE Q:pendingMessages gauge\n");
-    body.push_str("# TYPE Q:consumers gauge\n");
-    body.push_str("# TYPE T:pendingMessages gauge\n");
-    body.push_str("# TYPE T:subscribers gauge\n");
-    body.push_str("# TYPE T:durables gauge\n");
-    //get queues 
-    {
-      let c_map = queue::QUEUES.lock().unwrap();
-      for key in c_map.keys() {
-        let qinfo = c_map.get(key).unwrap();
-        let pending = format!("Q:pendingMessages{{queue=\"{}\" instance=\"EMS-ESB\"}} {}\n",qinfo.name,qinfo.pending_messages.unwrap());
-        let consumers = format!("Q:consumers{{queue=\"{}\" instance=\"EMS-ESB\"}} {}\n",qinfo.name,qinfo.consumer_count.unwrap());
-        body.push_str(&pending);
-        body.push_str(&consumers);
-      }
-    }
-    //get topics 
-    {
-      let c_map = topic::TOPICS.lock().unwrap();
-      for key in c_map.keys() {
-        let tinfo = c_map.get(key).unwrap();
-        let pending = format!("T:pendingMessages{{topic=\"{}\" instance=\"EMS-ESB\"}} {}\n",tinfo.name,tinfo.pending_messages.unwrap());
-        let subscribers = format!("T:subscribers{{topic=\"{}\" instance=\"EMS-ESB\"}} {}\n",tinfo.name,tinfo.subscriber_count.unwrap());
-        let durables = format!("T:durables{{topic=\"{}\" instance=\"EMS-ESB\"}} {}\n",tinfo.name,tinfo.durable_count.unwrap());
-        body.push_str(&pending);
-        body.push_str(&subscribers);
-        body.push_str(&durables);
-      }
-    }
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .header(CONTENT_TYPE, "text/plain; version=0.0.4; charset=utf-8")
-        .body(Body::from(body))
-        .unwrap();
-    Ok(response)
-  } else {
-    if uri.starts_with("/queue/"){
+
+// sort by usage, queue scaler, most used
+  let response = match uri {
+    x if x.starts_with("/queue/") => {
       let prefix_rm_uri = uri.strip_prefix("/queue/").unwrap();
       let mut json_string;
       let queue_name: String = prefix_rm_uri.replace("%7C", "|");
@@ -107,86 +72,120 @@ async fn respond(req: Request<Body>) -> Result<Response<Body>> {
           }
         }
       }
-      let response = Response::builder()
+      Response::builder()
       .status(StatusCode::OK)
       .header(CONTENT_TYPE, "application/json; charset=utf-8")
       .body(Body::from(json_string))
-      .unwrap();
-      Ok(response)
-    } else {
-      if uri.starts_with("/topic/") {
-        let prefix_rm_uri = uri.strip_prefix("/topic/").unwrap();
-        let mut json_string;
-        let topic_name: String = prefix_rm_uri.replace("%7C", "|");
-        if topic_name.contains("|") {
-          //multiple topics
-          let topic_list: Vec<&str> = topic_name.split("|").collect();
-          let all_topics = &mut TopicInfo{
-            name: "mixed".to_string(),
-            pending_messages: Some(0),
-            subscriber_count: Some(0),
-            durable_count: Some(0),
-            ..Default::default()
-          };
-          //get topics 
-          {
-            let c_map = topic::TOPICS.lock().unwrap();
-            let mut pending_messages = 0;
-            let mut subscriber_count = 0;
-            let mut durable_count = 0;
-            for key in c_map.keys() {
-              let tinfo = c_map.get(key).unwrap();
-              for t in &topic_list {
-                if t == &tinfo.name {
-                  pending_messages += tinfo.pending_messages.unwrap();
-                  subscriber_count += tinfo.subscriber_count.unwrap();
-                  durable_count += tinfo.durable_count.unwrap();
-                }
+      .unwrap()
+    },
+    "/metrics" => {
+      let mut body = "".to_owned();
+      body.push_str("# TYPE Q:pendingMessages gauge\n");
+      body.push_str("# TYPE Q:consumers gauge\n");
+      body.push_str("# TYPE T:pendingMessages gauge\n");
+      body.push_str("# TYPE T:subscribers gauge\n");
+      body.push_str("# TYPE T:durables gauge\n");
+      //get queues 
+      {
+        let c_map = queue::QUEUES.lock().unwrap();
+        for key in c_map.keys() {
+          let qinfo = c_map.get(key).unwrap();
+          let pending = format!("Q:pendingMessages{{queue=\"{}\" instance=\"EMS-ESB\"}} {}\n",qinfo.name,qinfo.pending_messages.unwrap());
+          let consumers = format!("Q:consumers{{queue=\"{}\" instance=\"EMS-ESB\"}} {}\n",qinfo.name,qinfo.consumer_count.unwrap());
+          body.push_str(&pending);
+          body.push_str(&consumers);
+        }
+      }
+      //get topics 
+      {
+        let c_map = topic::TOPICS.lock().unwrap();
+        for key in c_map.keys() {
+          let tinfo = c_map.get(key).unwrap();
+          let pending = format!("T:pendingMessages{{topic=\"{}\" instance=\"EMS-ESB\"}} {}\n",tinfo.name,tinfo.pending_messages.unwrap());
+          let subscribers = format!("T:subscribers{{topic=\"{}\" instance=\"EMS-ESB\"}} {}\n",tinfo.name,tinfo.subscriber_count.unwrap());
+          let durables = format!("T:durables{{topic=\"{}\" instance=\"EMS-ESB\"}} {}\n",tinfo.name,tinfo.durable_count.unwrap());
+          body.push_str(&pending);
+          body.push_str(&subscribers);
+          body.push_str(&durables);
+        }
+      }
+      Response::builder()
+          .status(StatusCode::OK)
+          .header(CONTENT_TYPE, "text/plain; version=0.0.4; charset=utf-8")
+          .body(Body::from(body))
+          .unwrap()
+    },
+    x if x.starts_with("/topic/") => {
+      let prefix_rm_uri = uri.strip_prefix("/topic/").unwrap();
+      let mut json_string;
+      let topic_name: String = prefix_rm_uri.replace("%7C", "|");
+      if topic_name.contains("|") {
+        //multiple topics
+        let topic_list: Vec<&str> = topic_name.split("|").collect();
+        let all_topics = &mut TopicInfo{
+          name: "mixed".to_string(),
+          pending_messages: Some(0),
+          subscriber_count: Some(0),
+          durable_count: Some(0),
+          ..Default::default()
+        };
+        //get topics 
+        {
+          let c_map = topic::TOPICS.lock().unwrap();
+          let mut pending_messages = 0;
+          let mut subscriber_count = 0;
+          let mut durable_count = 0;
+          for key in c_map.keys() {
+            let tinfo = c_map.get(key).unwrap();
+            for t in &topic_list {
+              if t == &tinfo.name {
+                pending_messages += tinfo.pending_messages.unwrap();
+                subscriber_count += tinfo.subscriber_count.unwrap();
+                durable_count += tinfo.durable_count.unwrap();
               }
             }
-            all_topics.pending_messages = Some(pending_messages);
-            all_topics.subscriber_count = Some(subscriber_count);
-            all_topics.durable_count = Some(durable_count);
           }
-          json_string = serde_json::to_string(all_topics).unwrap();
-        } else {
-          //get single topic 
-          let topic_info = &mut TopicInfo{
-            name: topic_name.to_string(),
-            pending_messages: Some(0),
-            subscriber_count: Some(0),
-            durable_count: Some(0),
-            ..Default::default()
-          };
-          json_string = serde_json::to_string(topic_info).unwrap();
-          {
-            let c_map = topic::TOPICS.lock().unwrap();
-            for key in c_map.keys() {
-              let tinfo = c_map.get(key).unwrap();
-              if tinfo.name == topic_name {
-                json_string = serde_json::to_string(tinfo).unwrap();
-              }
+          all_topics.pending_messages = Some(pending_messages);
+          all_topics.subscriber_count = Some(subscriber_count);
+          all_topics.durable_count = Some(durable_count);
+        }
+        json_string = serde_json::to_string(all_topics).unwrap();
+      } else {
+        //get single topic 
+        let topic_info = &mut TopicInfo{
+          name: topic_name.to_string(),
+          pending_messages: Some(0),
+          subscriber_count: Some(0),
+          durable_count: Some(0),
+          ..Default::default()
+        };
+        json_string = serde_json::to_string(topic_info).unwrap();
+        {
+          let c_map = topic::TOPICS.lock().unwrap();
+          for key in c_map.keys() {
+            let tinfo = c_map.get(key).unwrap();
+            if tinfo.name == topic_name {
+              json_string = serde_json::to_string(tinfo).unwrap();
             }
           }
         }
-        let response = Response::builder()
-        .status(StatusCode::OK)
-        .header(CONTENT_TYPE, "application/json; charset=utf-8")
-        .body(Body::from(json_string))
-        .unwrap();
-        Ok(response)
-      } else {
-        //unknown uri
-        error!("unkown endpoint: {}",uri);
-        let response = Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .header(CONTENT_TYPE, "text/plain; version=0.0.4; charset=utf-8")
-            .body(Body::from(""))
-            .unwrap();
-        Ok(response)
       }
+      Response::builder()
+      .status(StatusCode::OK)
+      .header(CONTENT_TYPE, "application/json; charset=utf-8")
+      .body(Body::from(json_string))
+      .unwrap()
+    },
+    _ => {
+      error!("unkown endpoint: {}",uri);
+      Response::builder()
+          .status(StatusCode::NOT_FOUND)
+          .header(CONTENT_TYPE, "text/plain; version=0.0.4; charset=utf-8")
+          .body(Body::from(""))
+          .unwrap()
     }
-  }
+  };
+  Ok(response)
 }
 
 pub fn init_admin_connection() -> Session{
