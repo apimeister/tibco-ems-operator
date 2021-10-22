2b096296ed33471cc82a35968d44870b3741a55f
Jens Walter <jens@apimeister.com>
2021-10-22 23:10:37 +0200

    #11 prepare scale to many

--- src/scaler/mod.rs
+++ src/scaler/mod.rs
@@ -7,31 +7,44 @@ use once_cell::sync::Lazy;
 use tokio::time::{self, Duration};
 use std::time::SystemTime;
 
+/// period to wait before a scale down can be performed
 const COOLDOWN_PERIOD_SECONDS: u64 = 60;
 
 pub type StateTrigger = (String, i64);
 /// TriggerMap contains of string (queue name) and i64 (outbound_message_count)
 type StateTriggerMap = HashMap<String, i64>;
 
+/// HashMap of Deployment Name with the value of Deployment State
+pub static KNOWN_STATES: Lazy<Mutex<HashMap<String,State>>> = Lazy::new(|| Mutex::new(HashMap::new()) );
+/// HashMap of Queue Name with the value as Vector of deployment names
+pub static SCALE_TARGETS: Lazy<Mutex<HashMap<String,Vec<String>>>> = Lazy::new(|| Mutex::new(HashMap::new()) );
+
+/// Represents the state of the k8s Deployment
 #[derive(Clone,Debug,PartialEq)]
 pub struct StateValue{
+  /// timestamp of the last update
   activity_timestamp: u64,
+  /// map of all triggering queues
   trigger: StateTriggerMap,
+  /// name of the k8s deployment
   deployment: String,
+  /// number of replicas
+  replicas: u32,
+  /// threshold for scaling
+  /// scaling is happening lineary, e.g. threshold 100 leads to the following behavoir:
+  /// 0 messages pending -> zero replicas
+  /// 1 message pending -> 1 replica
+  /// 100 message pending -> 2 replicas
+  /// 1000 messages pending -> 10 replicas
+  threshold: u32,
 }
+/// Represents the state of the k8s Deployment
 #[derive(Clone,Debug,PartialEq)]
 pub enum State{
+  /// deployment is scaled to 0 replicas
   Inactive(StateValue),
+  /// deployment is scaled to at least 1 replica
   Active(StateValue),
-}
-impl State {
-  pub fn new(deployment: String,trigger: StateTriggerMap) -> State {
-    State::Inactive(
-      StateValue{ 
-        activity_timestamp: get_epoch_seconds(), trigger, deployment
-      }
-    )
-  }
 }
 
 impl State {
@@ -58,6 +71,8 @@ impl State {
                 activity_timestamp: ts,
                 trigger: trigger_map,
                 deployment: val.deployment,
+                replicas: 1,
+                threshold: val.threshold,
               })    
           },
           Err(err) => {
@@ -67,6 +82,8 @@ impl State {
                 activity_timestamp: ts,
                 trigger: trigger_map,
                 deployment: val.deployment,
+                replicas: 0,
+                threshold: val.threshold,
               })
           },
         }
@@ -80,6 +97,8 @@ impl State {
             activity_timestamp: ts,
             trigger: trigger_map,
             deployment: val.deployment,
+            replicas: 1,
+            threshold: val.threshold,
           })
       },
     }
@@ -104,6 +123,8 @@ impl State {
             activity_timestamp: ts,
             trigger: trigger_map.clone(),
             deployment: val.deployment,
+            replicas: 1,
+            threshold: val.threshold,
           });
         }
         if val.activity_timestamp + COOLDOWN_PERIOD_SECONDS > ts {
@@ -126,6 +147,8 @@ impl State {
                 activity_timestamp: ts,
                 trigger: trigger_map.clone(),
                 deployment: val.deployment,
+                replicas: 0,
+                threshold: val.threshold,
               }),
           Err(err) => {
             error!("scale down failed: {}",err);
@@ -137,15 +160,11 @@ impl State {
   }
 }
 
-/// HashMap of Deployment Name with the value of Deployment State
-pub static KNOWN_STATES: Lazy<Mutex<HashMap<String,State>>> = Lazy::new(|| Mutex::new(HashMap::new()) );
-/// HashMap of Queue Name with the value as Vector of deployment names
-pub static SCALE_TARGETS: Lazy<Mutex<HashMap<String,Vec<String>>>> = Lazy::new(|| Mutex::new(HashMap::new()) );
-
 pub fn get_epoch_seconds() -> u64 {
   SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
 }
 
+/// watches for k8s Deployments with scaling labels present
 pub async fn run(){
   let client = Client::try_default().await.expect("getting default client");
   let namespace = env_var!(required "KUBERNETES_NAMESPACE");
@@ -174,6 +193,8 @@ pub async fn run(){
               activity_timestamp: get_epoch_seconds(),
               trigger:  val.trigger.clone(),
               deployment: deployment_name.clone(),
+              replicas: 0,
+              threshold: val.threshold,
             });
           },
           (State::Inactive(val), 1) => {
@@ -181,6 +202,8 @@ pub async fn run(){
               activity_timestamp: get_epoch_seconds(),
               trigger:  val.trigger.clone(),
               deployment: deployment_name.clone(),
+              replicas: 1,
+              threshold: val.threshold,
             });
           },
           _ => {
@@ -194,9 +217,11 @@ pub async fn run(){
         let d_name = deployment_name.clone();
         let mut trigger_map = StateTriggerMap::new();
         let labels = deployment.metadata.labels.unwrap();
-        for (key,queue_name) in labels {
+        let mut threshold = 100u32;
+        for (key,val) in labels {
           if key.starts_with("tibcoems.apimeister.com/queue") {
             //check known queues
+            let queue_name = val;
             let all_queues = super::queue::QUEUES.lock().unwrap();
             if all_queues.contains_key(&queue_name) {
               //known queue
@@ -218,18 +243,31 @@ pub async fn run(){
             }
             //add to trigger map
             trigger_map.insert(d_name.clone(),0);
+          }else if key.starts_with("tibcoems.apimeister.com/threshold") {
+            threshold = match val.parse::<u32>() {
+              Ok(result) => result,
+              Err(_err) => 100
+            };
           }
         }
         //check replica count and create new state object
         let deployment_state: State;
         let replica_count = deployment.spec.unwrap().replicas.unwrap();
         if replica_count == 0 {
-          deployment_state = State::new(deployment_name.clone(),trigger_map.clone());
+          deployment_state = State::Inactive(StateValue{
+            activity_timestamp: get_epoch_seconds(),
+            trigger:  trigger_map.clone(),
+            deployment: deployment_name.clone(),
+            replicas: 0,
+            threshold,
+          });
         }else{
           deployment_state = State::Active(StateValue{
             activity_timestamp: get_epoch_seconds(),
             trigger: trigger_map.clone(),
             deployment: deployment_name.clone(),
+            replicas: 1,
+            threshold,
           });
         }
         if !trigger_map.is_empty() {
